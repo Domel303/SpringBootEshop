9c3052e0c61aca81de0b08f5065b8c0f66266794
Domel303 <simacek.dominik@gmail.com>
2021-04-05 18:18:28 +0200

    Selenin tests

---
+++ src/test/java/cz/upce/eshop/ui/TestImplementation.java
@@ -0,0 +1,41 @@
+package cz.upce.eshop.ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestImplementation {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupWebdriverChromeDriver() {
+        String chromedriverpath = TestImplementation.class.getResource("/chromedriver.exe").getFile();
+        System.setProperty("webdriver.chrome.driver", chromedriverpath);
+    }
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void verifyGitHubTitle() {
+        driver.get("https://www.github.com");
+        assertThat(driver.getTitle(), containsString("GitHub"));
+    }
+}
